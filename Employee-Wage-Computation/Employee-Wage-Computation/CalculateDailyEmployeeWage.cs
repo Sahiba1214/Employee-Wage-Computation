6196c4fd4e0e307d0ef60565affde794db283291
sahiba <sahiba1214@gmail.com>
2021-11-15 14:04:08 +0530

    [sahiba1214] add : file added

---
+++ Employee-Wage-Computation/Employee-Wage-Computation/CalculateDailyEmployeeWage.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageComputation
+{
+    class CalculateDailyEmployeeWage
+    {
+        public const int WAGE_PR_HR = 20, FULL_DAY_HOUR = 8;
+        int totalSalary;
+        public void EmployeeDailyWag()
+        {
+            Random rand = new Random();
+            int empCheck = rand.Next(0, 2);
+            if (empCheck == 1)
+            {
+                this.totalSalary = WAGE_PR_HR * FULL_DAY_HOUR;
+                Console.WriteLine("Employee is Present and Salary is: " + this.totalSalary);
+            }
+            else
+                Console.WriteLine("Employee is Absent");
+        }
+    }
+}
